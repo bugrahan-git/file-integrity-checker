f37941c59e9b280b87fb59e63c6fba3c1997626e
bugrahan <umutbugrahanozkan@gmail.com>
2020-12-03 16:06:27 +0300

    add CL flags

--- src/ichecker.java
+++ src/ichecker.java
@@ -1,8 +1,37 @@
-public class ichecker {
+import java.util.Map;
+import java.util.HashMap;
 
-    public ichecker() {
+public class ichecker {
     
+    static String op;
+
+    public static void main(String ... args) {
+	
+	ichecker ic = new ichecker(); 
+	
+	Map<Character, String> argv = ic.getArgs(args);
+	
+	System.out.printf("Operation: %s\n", op);
+	for(Character c : argv.keySet()) {
+	    String key = c.toString();
+	    String value = argv.get(c);
+	    System.out.println(key + ": " + value);
+	}
     }
-    
-    
+
+
+    private static Map<Character, String> getArgs(String ... args) {
+	
+	op = args[0];
+	
+	final Map<Character, String> argv= new HashMap<>();
+	for(int i = 1; i < args.length; i+=2) {
+	    String tmp = args[i];
+	    if(tmp.charAt(0) == '-')
+		argv.put(tmp.charAt(1), args[i + 1]);
+	}
+
+	return argv;
+    }
+
 }
